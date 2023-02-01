4cd1f6632ec3afacd43b894e6bb77d19e43a1c5b
hiroto <chiroto.contact@gmail.com>
2023-02-01 17:55:52 +0900

    ログインの実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #redirect to user page
+      reset_session
+      log_in(user)
+      redirect_to user
+    else
+      #show error_message
+      flash.now[:danger] = "emailもしくはpasswordが正しくありません"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
