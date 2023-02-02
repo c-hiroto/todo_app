8613363a945bc5abc0a2b0aca58dc5a5a388df64
hiroto <chiroto.contact@gmail.com>
2023-02-02 19:36:11 +0900

    ユーザーの削除

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -39,4 +39,48 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              "password",
+                                            password_confirmation: "password",
+                                            admin: true } }
+    assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
+
+  test "delete link exists as admin" do
+    log_in_as(@user) #@user is admin in this case
+    get user_path(@other_user)
+    assert_select 'a[href=?]', user_path(@other_user), text: 'delete'
+    assert_difference 'User.count', -1 do
+      delete user_path(@other_user)
+      assert_response :see_other
+      assert_redirected_to root_url
+    end
+  end
+
+  test "delete link does not exist as non-admin" do
+    log_in_as(@other_user) #@other_user is non-admin in this case
+    get user_path(@user)
+    assert_select 'a', text: 'delete', count: 0
+  end
 end
