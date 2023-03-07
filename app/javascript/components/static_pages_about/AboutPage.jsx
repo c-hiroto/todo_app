2132f7f719e4659e36b22201975264599b1bfccf
hiroto <chiroto.contact@gmail.com>
2023-03-07 20:32:38 +0900

    static-pages-aboutをReactで記述

---
+++ app/javascript/components/static_pages_about/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+export const AboutPage = () => {
+    render(
+    <>
+        <h1>About</h1>
+        <h2>このアプリケーションは個人の勉強管理用です</h2>
+    </>    
+    )
+}
---
+++ app/javascript/staticPagesAbout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { AboutPage } from "./components/static_pages_about/AboutPage";
+
+
+const about = document.getElementById('about');
+createRoot(about).render(
+  <React.StrictMode>
+    <AboutPage />
+  </React.StrictMode>
+  );
