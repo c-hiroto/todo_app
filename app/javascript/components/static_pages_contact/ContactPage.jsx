c46d21ecce30933e25321e86c1a15085fd1ac8c7
hiroto <chiroto.contact@gmail.com>
2023-03-07 20:33:06 +0900

    static-pages-contactをReactで記述

---
+++ app/javascript/components/static_pages_contact/ContactPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "react-dom";
+
+export const ContactPage = () => {
+    render(
+    <>
+        <h1>Contact</h1>
+        <p>
+            Contact someone about this app at the
+            <a href="#">contact page</a>.
+        </p>
+    </>    
+    )
+}
---
+++ app/javascript/staticPagesContact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { ContactPage } from "./components/static_pages_contact/ContactPage";
+
+
+const contact = document.getElementById('contact');
+createRoot(contact).render(
+  <React.StrictMode>
+    <ContactPage />
+  </React.StrictMode>
+  );
